6ee83bc844ef6c286abd097125373692010ad6b4
Naoki0722 <mattu.nao722@gmail.com>
2022-06-08 20:44:48 +0900

    first commit

---
+++ lib/posts.ts
@@ -0,0 +1,14 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+
+const postsDirectory = path.join(process.cwd(), 'posts');
+
+export function getSortedPostsData() {
+    const fileNames = fs.readdirSync(postsDirectory);
+    const allPostsData = fileNames.map((fileName) => {
+        const id = fileName.replace(/\.md$/, 'utf8')
+
+        // const matterResult = matter(fileContnts)
+    })
+}
